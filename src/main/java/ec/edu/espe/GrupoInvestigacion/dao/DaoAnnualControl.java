3e76d9e181f43dde0c47fc3baa0a7966538f5ebf
Santiago David <sdquishpe1@espe.edu.ec>
2024-10-25 11:22:45 -0500

    correccion

--- src/main/java/ec/edu/espe/GrupoInvestigacion/dao/DaoAnnualControl.java
+++ src/main/java/ec/edu/espe/GrupoInvestigacion/dao/DaoAnnualControl.java
@@ -1,23 +1,24 @@
 package ec.edu.espe.GrupoInvestigacion.dao;
 
 import ec.edu.espe.GrupoInvestigacion.model.ModelAnnualControl;
-import ec.edu.espe.GrupoInvestigacion.model.ModelDeveLega;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface DaoAnnualControl extends CrudRepository<ModelAnnualControl, Long> {
-    @Query(value="SELECT ac FROM ModelAnnualControl ac WHERE ac.modelAnnualOperativePlan.id=:id")
-    public Optional<List<ModelAnnualControl>> findByIdPlan(Long id);
 
-    @Query(value="SELECT ac FROM ModelAnnualControl ac WHERE ac.modelControlPanel.id=:id")
-    public Optional<List<ModelAnnualControl>> findByIdControl(Long id);
+    @Query("SELECT ac FROM ModelAnnualControl ac WHERE ac.modelAnnualOperativePlan.id = :id")
+    Optional<List<ModelAnnualControl>> findByIdPlan(@Param("id") Long id);
 
-    @Query(value = "SELECT ac FROM ModelAnnualControl ac WHERE ac.modelControlPanel.id=:idPanel AND ac.modelAnnualOperativePlan.id=:idPlan ")    public Optional<List<ModelAnnualControl>> findByIdPanel(Long id);
-    public Optional<ModelAnnualControl> findByIdControlPlan(Long idPanel,Long idPlan);
+    @Query("SELECT ac FROM ModelAnnualControl ac WHERE ac.modelControlPanel.id = :id")
+    Optional<List<ModelAnnualControl>> findByIdControl(@Param("id") Long id);
 
+    @Query("SELECT ac FROM ModelAnnualControl ac WHERE ac.modelControlPanel.id = :idPanel AND ac.modelAnnualOperativePlan.id = :idPlan")
+    Optional<List<ModelAnnualControl>> findByIdPanel(@Param("idPanel") Long idPanel, @Param("idPlan") Long idPlan);
 
-
+    @Query("SELECT ac FROM ModelAnnualControl ac WHERE ac.modelControlPanel.id = :idPanel AND ac.modelAnnualOperativePlan.id = :idPlan")
+    Optional<ModelAnnualControl> findByIdControlPlan(@Param("idPanel") Long idPanel, @Param("idPlan") Long idPlan);
 }
